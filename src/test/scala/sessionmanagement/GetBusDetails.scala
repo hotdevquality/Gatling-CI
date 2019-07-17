7ce50d76a4529f2536e63883fe677442ae1fd7d5
Timothy Hassan <timobrownng@gmail.com>
2019-07-17 23:21:44 +0100

    added global assertions

--- src/test/scala/sessionmanagement/GetBusDetails.scala
+++ src/test/scala/sessionmanagement/GetBusDetails.scala
@@ -23,23 +23,27 @@ class GetBusDetails extends Simulation {
       .get("http://developer.goibibo.com/api/bus/search/?app_id=" + appId + "&app_key=" + appKey + "&format=json&source=${source}&destination=${destination}&dateofdeparture=" + depatDate + "")
       //        .check(jsonPath("$.data..skey").findAll.saveAs("searchKey")))
       .check(jsonPath("$.data..skey").find.saveAs("searchKey")))
-    //    .check(jsonPath("$.data..skey").find(2).saveAs("searchKey")))
-    // $.data.[0].skey // $.data.onwardflights[0].skey //return first index
-    // $.data..skey // returns all occurrence
-    //      .exec(session => {
-    //          println(session)
-    //          session
-    //      })
+  //    .check(jsonPath("$.data..skey").find(2).saveAs("searchKey")))
+  // $.data.[0].skey // $.data.onwardflights[0].skey //return first index
+  // $.data..skey // returns all occurrence
+  //      .exec(session => {
+  //          println(session)
+  //          session
+  //      })
 
-    .exec(session => {
-    val searchVal = session("searchKey")
-    println(searchVal.as[String])
-    println(searchVal.asOption[String])
-    println(searchVal.validate[String])
-    session
-  })
+  //    .exec(session => {
+  //    val searchVal = session("searchKey")
+  //    println(searchVal.as[String])
+  //    println(searchVal.asOption[String])
+  //    println(searchVal.validate[String])
+  //    session
+  //  })
 
-  setUp(scn.inject(atOnceUsers(1))) protocols (httpConfig)
-
-  //curl -v  -X GET "http://developer.goibibo.com/api/bus/seatmap/?app_id=796bdc4e&app_key=694b0a0f930576bf40d4aa4d14c69ed0&format=json&skey=vJ52KC0ymd0635qTD9bDDy9GHBkGl5FJMJjd0aFP-WYUy-0C4NoQ9XHUgKaU_iClb2vn0uI5LrP7ZtAp"
+  //  setUp(scn.inject(atOnceUsers(2))) protocols (httpConfig)
+  setUp(scn.inject(atOnceUsers(2)).protocols(httpConfig))
+    .assertions(
+      global.responseTime.max.lt(50),
+//      global.successfulRequests.percent.gt(95)
+      global.successfulRequests.percent.is(100)
+    )
 }
